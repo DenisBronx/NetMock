724c99149771daa6c51e3957230c9570230709b2
Denis <dnsbrnd@gmail.com>
2026-02-17 20:04:58 +0000

    Fixed references for CI and use gradle properties instead of parsing secrets

--- convention-plugins/src/main/kotlin/convention.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/convention.publication.gradle.kts
@@ -6,38 +6,6 @@ plugins {
     signing
 }
 
-// Stub secrets to let the project sync and build without the publication values set up
-ext["signing.keyId"] = null
-ext["signing.password"] = null
-ext["signing.secretKeyRingFile"] = null
-ext["signing.secretKey"] = null
-ext["signingInMemoryKeyId"] = null
-ext["signingInMemoryKey"] = null
-ext["signingInMemoryKeyPassword"] = null
-ext["mavenCentralUsername"] = null
-ext["mavenCentralPassword"] = null
-ext["signing.secretKeyRingFile"] = null
-
-// Load secrets from secrets.properties or environment (CI)
-val secretPropsFile = project.rootProject.file("secrets.properties")
-if (secretPropsFile.exists()) {
-    Properties().apply {
-        secretPropsFile.reader().use { load(it) }
-    }.forEach { (name, value) ->
-        ext[name.toString()] = value
-    }
-} else {
-    ext["signing.keyId"] = System.getenv("SIGNING_KEY_ID")
-    ext["signing.password"] = System.getenv("SIGNING_PASSWORD")
-    ext["signing.secretKey"] = System.getenv("SIGNING_SECRET_KEY")
-    ext["signingInMemoryKeyId"] = System.getenv("SIGNING_KEY_ID")
-    ext["signingInMemoryKey"] = System.getenv("GPG_KEY_CONTENTS")
-    ext["signingInMemoryKeyPassword"] = System.getenv("SIGNING_PASSWORD")
-    ext["mavenCentralUsername"] = System.getenv("MAVEN_CENTRAL_USERNAME")
-    ext["mavenCentralPassword"] = System.getenv("MAVEN_CENTRAL_PASSWORD")
-    ext["signing.secretKeyRingFile"] = System.getenv("SIGNING_SECRET_KEY_RING_FILE")
-}
-
 mavenPublishing {
     publishToMavenCentral()
 
