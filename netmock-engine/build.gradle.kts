f3cb33bccdeb672b6630fa508f7f93190b2ac888
Denis <dnsbrnd@gmail.com>
2026-08-24 11:55:44 +0100

    Updated Readme and targets

--- netmock-engine/build.gradle.kts
+++ netmock-engine/build.gradle.kts
@@ -26,14 +26,11 @@ kotlin {
             useJUnit()
         }
     }
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
+    mingwX64()
+    macosX64()
+    macosArm64()
+    linuxX64()
+    linuxArm64()
     iosX64()
     iosArm64()
     iosSimulatorArm64()
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -e
-./gradlew jsTest
-./gradlew wasmJsTest
+./gradlew test
 ./gradlew koverXmlReportCustom
 ./scripts/testCoverageValidator.sh
